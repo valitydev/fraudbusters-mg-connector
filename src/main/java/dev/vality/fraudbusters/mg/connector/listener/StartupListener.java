a8eaac90f256c467076d5d80ea647c70c98669c8
kostyastruga <koztyastruga@gmail.com>
2024-08-23 11:15:59 +0300

    Add enabled parameters

--- src/main/java/dev/vality/fraudbusters/mg/connector/listener/StartupListener.java
+++ src/main/java/dev/vality/fraudbusters/mg/connector/listener/StartupListener.java
@@ -33,16 +33,12 @@ public class StartupListener implements ApplicationListener<ContextRefreshedEven
     }
 
     private void initKafkaStream(EventSinkFactory eventSinkFactory) {
-        if (eventSinkFactory.isEnabled()) {
             KafkaStreams kafkaStreams = eventSinkFactory.create();
             kafkaStreams.start();
             Runtime.getRuntime()
                     .addShutdownHook(new Thread(() -> kafkaStreams.close(Duration.ofSeconds(cleanTimeoutSec))));
             eventSinkStreamsPool.put(eventSinkFactory.getType(), kafkaStreams);
             log.info("StartupListener start stream kafkaStreams: {}", kafkaStreams.allMetadata());
-        } else {
-            log.info("StartupListener eventSinkFactory: {} not enabled", eventSinkFactory);
-        }
     }
 
 }
